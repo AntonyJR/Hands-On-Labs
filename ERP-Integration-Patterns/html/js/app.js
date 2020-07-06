6a8d9cf91a4bb378aa2cb79cccafac6b7bfb3de9
Nathan Angstadt <nathan.angstadt@me.com>
2020-07-06 16:39:39 -0600

    Walkthrough review changes

--- ERP-Integration-Patterns/html/js/app.js
+++ ERP-Integration-Patterns/html/js/app.js
@@ -70,7 +70,7 @@ require(['ojs/ojcore', 'knockout', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtool
         new OICEnvironmentRegion("London", "ld"),
       ]),
       self.selectedOICEnvironmentRegion = ko.observable()
-/**
+
       class OICEnvironment {
         constructor(name, baseURL, url, usernamePrefix) {
           this.name = name;
@@ -80,14 +80,17 @@ require(['ojs/ojcore', 'knockout', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtool
         }
       }
       self.availableOICEnvironments = ko.observableArray([
-        new OICEnvironment("Training 1", "https://oictraining1-oicpm.integration.ocp.oraclecloud.com", "https://oictraining1-oicpm.integration.ocp.oraclecloud.com/ic/home/", "OICTraining1ic"),
-        new OICEnvironment("Training 2", "https://oictraining2-oicpm.integration.ocp.oraclecloud.com", "https://oictraining2-oicpm.integration.ocp.oraclecloud.com/ic/home/", "OICTraining2ic"),
-        new OICEnvironment("Training 3", "https://oictraining3-oicpm.integration.ocp.oraclecloud.com", "https://oictraining3-oicpm.integration.ocp.oraclecloud.com/ic/home/", "OICTraining3ic"),
-        new OICEnvironment("SC Training 1", "https://oicsctraining1-oicpm.integration.ocp.oraclecloud.com", "https://oicsctraining1-oicpm.integration.ocp.oraclecloud.com/ic/home/", "oicuserscA"),
-        new OICEnvironment("SC Training 2", "https://oicsctraining2-oicpm.integration.ocp.oraclecloud.com", "https://oicsctraining2-oicpm.integration.ocp.oraclecloud.com/ic/home/", "oicuserscB")
+        new OICEnvironment("Training 1", "https://oictraining1-oicpm-px.integration.ocp.oraclecloud.com", "https://oictraining1-oicpm-px.integration.ocp.oraclecloud.com/ic/home", "oicuserscA"),
+        new OICEnvironment("Training 2", "https://oictraining2-oicpm-px.integration.ocp.oraclecloud.com", "https://oictraining2-oicpm-px.integration.ocp.oraclecloud.com/ic/home", "oicuserscB"),
+        new OICEnvironment("Training 3", "https://oictraining3-oicpm-px.integration.ocp.oraclecloud.com", "https://oictraining3-oicpm-px.integration.ocp.oraclecloud.com/ic/home", "oicuserscA"),
+        new OICEnvironment("Training 4", "https://oictraining4-oicpm-px.integration.ocp.oraclecloud.com", "https://oictraining4-oicpm-px.integration.ocp.oraclecloud.com/ic/home", "oicuserscB")
+        /**
+        ,
+        new OICEnvironment("Gold", "https://training-gold-oicpm-px.integration.ocp.oraclecloud.com", "https://training-gold-oicpm-px.integration.ocp.oraclecloud.com/ic/home", "oicuserscB")
+        **/
       ]),
       self.selectedOICEnvironment = ko.observable()
-**/
+
       class AppEnvironment {
         constructor(name, url, usernamePrefix) {
           this.name = name;
@@ -96,11 +99,8 @@ require(['ojs/ojcore', 'knockout', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtool
         }
       }      
       self.availableAppEnvironments = ko.observableArray([
-        new AppEnvironment("ERP Cloud Testing - zmyc",    "https://ucf1-zmyc-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Redwood", "casey.brown"),
-        new AppEnvironment("ERP Cloud Purchasing - zjie", "https://adc2-zjie-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "casey.brown"),
-        new AppEnvironment("ERP Cloud Payables - zjie",   "https://adc2-zjie-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "bala.gupta"),
-        new AppEnvironment("ERP Cloud Purchasing - ziyj", "https://adc4-ziyj-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "casey.brown"),
-        new AppEnvironment("ERP Cloud Payables - ziyj",   "https://adc4-ziyj-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "bala.gupta"),
+        new AppEnvironment("ERP Cloud Purchasing - zoml", "https://ucf1-zoml-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "casey.brown"),
+        new AppEnvironment("ERP Cloud Payables - zoml",   "https://ucf1-zoml-fa-ext.oracledemos.com/fscmUI/faces/FuseWelcome?fndThemeName=Vision_Newsfeed", "bala.gupta")
       ]),
       self.selectedAppEnvironment = ko.observable()
 
